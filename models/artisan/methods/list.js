864d052131f2ae8750ba8d8bb4f26f24a8c4ef6e
Abel <abel@chalier.me>
2015-05-18 16:54:18 +0200

    autocommit

--- models/artisan/methods/list.js
+++ models/artisan/methods/list.js
@@ -4,44 +4,47 @@ var async = require('async');
 
 module.exports = function(schema) {
 
-  var selectedFields = [
-    '-_id',
-    'id',
-    'address',
-    'nomSociete',
-    'categories'
-  ]
-  var s = "";
-  selectedFields.forEach(function(e)  {
-    s += (' ' + e);
-  })
-  schema.statics.list = function(req, res) {
-    var _this = this;
-    return new Promise(function(resolve, reject) {
-      redis.get('artisanList', function(err, reply) {
-        if (!err && reply && !req.query.cache) {
-          return resolve(JSON.parse(reply));
-        }
-        _this.model('artisan').find({'categories':'PL', archive:false}).sort('-id').select(s).then(function(docs) {
-          async.map(docs, function(e, cb) {
-            cb(null, {
-              id: e.id,
-              n: e.nomSociete,
-              c: e.categories,
-              add: {
-                lt: e.address.lt,
-                lg: e.address.lg
-              }
+    var selectedFields = [
+        '-_id',
+        'id',
+        'address',
+        'nomSociete',
+        'categories'
+    ]
+    var s = "";
+    selectedFields.forEach(function(e)  {
+        s += (' ' + e);
+    })
+    schema.statics.list = function(req, res) {
+        var _this = this;
+        return new Promise(function(resolve, reject) {
+            redis.get('artisanList', function(err, reply) {
+                if (!err && reply && !req.query.cache) {
+                    return resolve(JSON.parse(reply));
+                }
+                _this.model('artisan').find({
+                    'categories': 'PL',
+                    archive: false
+                }).sort('-id').select(s).then(function(docs) {
+                    async.map(docs, function(e, cb) {
+                        cb(null, {
+                            id: e.id,
+                            n: e.nomSociete,
+                            c: e.categories,
+                            add: {
+                                lt: e.address.lt,
+                                lg: e.address.lg
+                            }
+                        });
+                    }, function(err, result)  {
+                        resolve(result);
+                        //console.timeEnd('interList')
+                        //console.log('nocache')
+                        redis.set("artisanList", JSON.stringify(result))
+                        redis.expire("artisanList", 6000)
+                    });
+                })
             });
-          }, function(err, result)  {
-            resolve(result);
-            //console.timeEnd('interList')
-            //console.log('nocache')
-            redis.set("artisanList", JSON.stringify(result))
-            redis.expire("artisanList", 6000)
-          });
-        })
-      });
-    });
-  }
+        });
+    }
 }
