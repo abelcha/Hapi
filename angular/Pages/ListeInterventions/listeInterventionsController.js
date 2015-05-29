fdabf5ca3491cb35e64f6d9a1990b063ea0e30bb
Abel <abel@chalier.me>
2015-05-29 11:44:06 +0200

    autocommit

--- angular/Pages/ListeInterventions/listeInterventionsController.js
+++ angular/Pages/ListeInterventions/listeInterventionsController.js
@@ -81,7 +81,7 @@ angular.module('edison').controller('InterventionsController', function(tabConta
             } else {
                 $q.all([
                     edisonAPI.intervention.get(inter.id),
-                    edisonAPI.artisan.GetStats(inter.ai)
+                    edisonAPI.artisan.getStats(inter.ai)
                 ]).then(function(result)  {
 
                     $rootScope.expendedRow = inter.id;
