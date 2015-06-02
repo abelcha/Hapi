f734d4f2867509e025846d5b22998d645c43785c
Abel <abel@chalier.me>
2015-06-03 00:41:33 +0200

    test ssh

--- server/edison_components/db.js
+++ server/edison_components/db.js
@@ -26,6 +26,7 @@ module.exports = function() {
     })
     mongoose.utils = {
         round: function(field) {
+            
             return {
                 $divide: [{
                         $subtract: [{
