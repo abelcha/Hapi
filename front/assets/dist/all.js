4f829e20a278b7ded76e1551bb1b42bdd5f8f0bf
Abel <abel@chalier.me>
2015-07-13 16:54:24 +0200

    autocommit

--- front/assets/dist/all.js
+++ front/assets/dist/all.js
@@ -421,21 +421,24 @@ angular.module('edison').directive('artisanRecap', function(edisonAPI, config, $
         },
         link: function(scope, element, attrs) {
             var reload = function() {
-                $("#chartContainer").empty()
-                edisonAPI.artisan.extendedStats(scope.id).success(function(resp) {
-                    var svg = dimple.newSvg("#chartContainer", 600, 200);
-                    var myChart = new dimple.chart(svg, resp);
-                    myChart.setBounds(60, 30, 380, 120)
-                    var x = myChart.addCategoryAxis("x", "date");
-                    x.addOrderRule("dt");
-                    myChart.addMeasureAxis("y", "total");
-                    myChart.addSeries("status", dimple.plot.bar);
-                    myChart.addLegend(60, 10, 410, 20, "right");
-                    myChart.assignColor("ANN", "#F44336");
-                    myChart.assignColor("ENC", "#FDD835");
-                    myChart.assignColor("VRF", "#4CAF50");
-                    myChart.draw();
-                })
+              //  $("#chartContainer").empty()
+                if (myChart)
+                    edisonAPI.artisan.extendedStats(scope.id).success(function(resp) {
+                        var svg;
+                        if (!svg)
+                            svg = dimple.newSvg("#chartContainer", 1000, 200);
+                        var myChart = new dimple.chart(svg, resp);
+                        myChart.setBounds(60, 30, 700, 120)
+                        var x = myChart.addCategoryAxis("x", "date");
+                        x.addOrderRule("dt");
+                        myChart.addMeasureAxis("y", "total");
+                        myChart.addSeries("status", dimple.plot.bar);
+                        myChart.addLegend(60, 10, 680, 20, "right");
+                        myChart.assignColor("ANN", "#F44336");
+                        myChart.assignColor("ENC", "#FDD835");
+                        myChart.assignColor("VRF", "#4CAF50");
+                        myChart.draw();
+                    })
             }
             reload()
             scope.$watch('id', function(current, prev) {
@@ -445,6 +448,7 @@ angular.module('edison').directive('artisanRecap', function(edisonAPI, config, $
         }
     };
 });
+
  angular.module('edison').directive('box', [function() {
      "use strict";
      return {
