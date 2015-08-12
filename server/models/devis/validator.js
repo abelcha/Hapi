ca3316377f714d4c2d1376f213964540aaaee370
Abel <abel@chalier.me>
2015-08-12 14:49:18 +0200

    autocommit

--- server/models/devis/validator.js
+++ server/models/devis/validator.js
@@ -19,19 +19,20 @@ module.exports = function(schema) {
         this.client.address.v = upper(this.client.address.v)
         next();
     });
-    
+
 
     schema.post('save', function(doc) {
         if (!isWorker) {
             redis.del('interventionStats', function() {
                 db.model('devis').cacheActualise(doc);
             });
-        }
-        if (envProd) {
-            var v1 = new V1(doc, true);
-            v1.send(function(resp) {
-                console.log('=====-->', resp)
-            });
+            if (envProd) {
+                console.log('v1 translate');
+                var v1 = new V1(doc, true);
+                v1.send(function(resp) {
+                    console.log('=====-->', resp)
+                });
+            }
         }
     })
 
