9dafbaf8091717d816b1a923e65f03c27d6d3926
Abel <abel@chalier.me>
2015-08-12 15:28:45 +0200

    autocommit

--- server/models/devis/validator.js
+++ server/models/devis/validator.js
@@ -27,7 +27,7 @@ module.exports = function(schema) {
                 db.model('devis').cacheActualise(doc);
             });
             if (envProd) {
-                console.log('v1 translate');
+                console.log('v1 translate' + doc.id);
                 var v1 = new V1(doc, true);
                 v1.send(function(resp) {
                     console.log('=====-->', resp)
