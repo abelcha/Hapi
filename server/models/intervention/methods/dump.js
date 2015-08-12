820a92e33721731a52421a885022d5a2443473c1
Abel <abel@chalier.me>
2015-08-12 07:38:51 +0200

    autocommit

--- server/models/intervention/methods/dump.js
+++ server/models/intervention/methods/dump.js
@@ -330,6 +330,7 @@
         }
 
         var dumpOne = function(id) {
+            console.log('dumpOne', id)
             return new Promise(function(resolve, reject) {
                 request.get(key.alvin.url + "/dumpIntervention.php?id=" + id + "&key=" + key.alvin.pass, function(err, resp, body) {
                     if (err || resp.statusCode !== 200 || !body || body == 'null') {
