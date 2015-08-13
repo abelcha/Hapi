db5e35cf66e19597f4b048a1273596a9a9ab8ddb
Abel <abel@chalier.me>
2015-08-13 21:27:18 +0200

    autocommit

--- server/edison_components/redis.js
+++ server/edison_components/redis.js
@@ -1,27 +1,31 @@
 module.exports = function() {
     var redis = require("redis");
     var redisClient;
-    console.log(envStaging, process.env.REDISTOGO_URL)
-    if (envProd || envStaging) {
-        //console.log('here')
-        var url = require('url');
-        var redisURL = url.parse(process.env.REDISTOGO_URL);
-        redisClient = redis.createClient(redisURL.port, redisURL.hostname, {
-            no_ready_check: true
+    try {
+
+        if (envProd || envStaging) {
+            //console.log('here')
+            var url = require('url');
+            var redisURL = url.parse(process.env.REDISTOGO_URL);
+            redisClient = redis.createClient(redisURL.port, redisURL.hostname, {
+                no_ready_check: true
+            });
+            var stack = new Error().stack
+            console.log(stack)
+                //console.log(redisClient)
+            redisClient.auth(redisURL.auth.split(":")[1]);
+
+        } else {
+            redisClient = redis.createClient();
+        }
+
+
+        redisClient.on("error", function(err) {
+            console.log("Redis Error " + err);
         });
-        var stack = new Error().stack
-        console.log(stack)
-        //console.log(redisClient)
-        redisClient.auth(redisURL.auth.split(":")[1]);
 
-    } else {
-        redisClient = redis.createClient();
+    } catch (e) {
+        __catch(e);
     }
-
-
-    redisClient.on("error", function(err) {
-        console.log("Redis Error " + err);
-    });
-
     return redisClient;
 }
