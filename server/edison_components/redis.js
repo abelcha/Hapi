d0cd0ec3090a825901e3a81263d643a17dabe69b
Abel <abel@chalier.me>
2015-08-13 21:24:08 +0200

    autocommit

--- server/edison_components/redis.js
+++ server/edison_components/redis.js
@@ -3,13 +3,15 @@ module.exports = function() {
     var redisClient;
     console.log(envStaging, process.env.REDISTOGO_URL)
     if (envProd || envStaging) {
-      console.log('here')
+        //console.log('here')
         var url = require('url');
         var redisURL = url.parse(process.env.REDISTOGO_URL);
         redisClient = redis.createClient(redisURL.port, redisURL.hostname, {
             no_ready_check: true
         });
-        console.log(redisClient)
+        var stack = new Error().stack
+        console.log(stack)
+        //console.log(redisClient)
         redisClient.auth(redisURL.auth.split(":")[1]);
 
     } else {
