ec2bea7ad02803fb52b57bb4df20dd43116a8945
Abel <abel@chalier.me>
2015-08-17 20:06:58 +0200

    autocommit

--- server/app.js
+++ server/app.js
@@ -29,9 +29,10 @@ global.__catch = function(e) {
 var key = requireLocal('config/_keys');
 var dep = require(process.cwd() + '/server/loadDependencies');
 global.edison = dep.loadDir(process.cwd() + "/server/edison_components");
-global.envProd = process.env.NODE_ENV === "production";
-global.envDev = process.env.NODE_ENV === "developement";
-global.envStaging = process.env.NODE_ENV === "staging";
+global.envProd = process.env.APP_ENV === "PRODUCTION";
+global.envDev = process.env.APP_ENV === "DEVELOPMENT";
+global.envStaging = process.env.APP_ENV === "STAGING";
+console.log(envProd, envDev, envStaging)
 global.redis = edison.redis();
 global.db = edison.db();
 global.sms = new edison.mobyt(key.mobyt.login, key.mobyt.pass);
