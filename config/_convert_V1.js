468e203941065b43710c1e73ceafdb4b4a58ab54
Abel <abel@chalier.me>
2015-08-18 17:05:41 +0200

    autocommit

--- config/_convert_V1.js
+++ config/_convert_V1.js
@@ -23,12 +23,12 @@ var V1 = function(d, devis, legacy) {
             x.date_intervention = dateIntervention.format('DD/MM/YYYY')
             x.date_intervention_en = dateIntervention.format('YYYYMMDD')
             x.heure_intervention = dateIntervention.format('HH:mm:ss')
-            x.t_stamp_intervention = dateIntervention.unix() + ms.hour(2)
+            x.t_stamp_intervention = dateIntervention.unix() + ms.hours(2)
         }
         if (d.compta.reglement.date) {
             x.date_paiement_client = moment(new Date(d.compta.reglement.date)).format('DD/MM/YYYY')
         }
-        x.t_stamp = dateAjout.unix() + ms.hour(2)
+        x.t_stamp = dateAjout.unix() + ms.hours(2)
         x.date_ajout = dateAjout.format('DD/MM/YYYY')
         x.heure_ajout = dateAjout.format('HH:mm:ss')
         x.date_ajout_en = dateAjout.format('YYYYMMDD')
