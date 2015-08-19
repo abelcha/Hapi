dbb349c2e8ecd5ebe31e63804f1e86e05172a35d
Abel <abel@chalier.me>
2015-08-19 16:34:54 +0200

    autocommit

--- server/worker.js
+++ server/worker.js
@@ -16,48 +16,45 @@ try {
     if (envProd ||  envStaging) {
         var redisUrl = url.parse(process.env.REDISTOGO_URL);
     }
-    redis.keys("kue*", function(err, re) {
-        re.forEach(function(k) {
-            redis.del(k);
+    redis.delWildcard("kue".envify() + '*', function() {
+
+        var jobs = kue.createQueue({
+            prefix: 'kue'.envify(),
+            redis: envProd || envStaging ? {
+                port: redisUrl.port,
+                host: redisUrl.hostname,
+                auth: redisUrl.auth.split(":")[1],
+            } : undefined,
+            disableSearch: true
         });
-    });
+
+        jobs.process('db', function(job, done) {
+            var terminated = false
+            console.log(job.data.model, job.data.method)
+            db.model(job.data.model)[job.data.method](job.data.arg).then(function(result)  {
+                terminated = true;
+                console.log("job success")
+                done(null, result);
+            }, function(err) {
+                console.log("job error", JSON.stringify(err, undefined, 1));
+                return done(err ||  "error");
+            })
+        });
 
 
-    var jobs = kue.createQueue({
-        prefix: 'kue'.envify(),
-        redis: envProd || envStaging ? {
-            port: redisUrl.port,
-            host: redisUrl.hostname,
-            auth: redisUrl.auth.split(":")[1],
-        } : undefined,
-        disableSearch: true
-    });
+        jobs.process('db_id', function(job, done) {
+            console.log(job.data.model, job.data.method)
+            db.model(job.data.model)[job.data.method].fn(job.data.data, job.data.req).then(function(result)  {
+                console.log("job success")
+                done(null, result);
+            }, function(err) {
+                console.log("job error", err);
+                return done(err ||  "error");
+            }).catch(__catch)
+        });
 
+    })
 
-    jobs.process('db', function(job, done) {
-        var terminated = false
-        console.log(job.data.model, job.data.method)
-        db.model(job.data.model)[job.data.method](job.data.arg).then(function(result)  {
-            terminated = true;
-            console.log("job success")
-            done(null, result);
-        }, function(err) {
-            console.log("job error", JSON.stringify(err, undefined, 1));
-            return done(err ||  "error");
-        })
-    });
-
-
-    jobs.process('db_id', function(job, done) {
-        console.log(job.data.model, job.data.method)
-        db.model(job.data.model)[job.data.method].fn(job.data.data, job.data.req).then(function(result)  {
-            console.log("job success")
-            done(null, result);
-        }, function(err) {
-            console.log("job error", err);
-            return done(err ||  "error");
-        }).catch(__catch)
-    });
 } catch (e) {
     __catch(e)
 }
