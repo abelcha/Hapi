d86f8a26d767033ae75b6d85567d4e3a382ccc1a
Abel Chalier <abelchalier@Abels-MacBook-Pro.local>
2015-09-08 17:49:06 +0200

    autocommit

---
+++ server/models/intervention/methods/vcf.js
@@ -0,0 +1,34 @@
+module.exports = function(schema) {
+
+    schema.statics.vcf = function(req, res) {
+        var vCard = require('cozy-vcard')
+        var fs = require('fs')
+        var config = requireLocal('config/dataList')
+        var _ = require('lodash')
+        return new Promise(function(resolve, reject) {
+
+            db.model('intervention').find().limit(req.query.limit || 1000).then(function(docs) {
+                var rtn = "";
+                _.each(docs, function(e) {
+
+                    e.__cat = config.categories[e.categorie].long_name
+                    rtn += "BEGIN:VCARD\n";
+                    rtn += "VERSION:3.0\n" +
+                        _.template("N: {{id}} {{client.nom}} {{client.prenom}} - {{client.address.cp}} {{client.address.v}} - {{__cat}}\n")(e) +
+                        //"N: 27285 CEYLAN IBRAHIM - 60160 MONTATAIRE - VITRERIE\n" + 
+                        //"FN: 27285 - CEYLAN IBRAHIM - 60160 MONTATAIRE - VITRERIE\n" + 
+                        _.template("FN: {{id}} {{client.nom}} {{client.prenom}} - {{client.address.cp}} {{client.address.v}} - {{__cat}}\n")(e) +
+                        "TEL;\n" +
+                        "WORK;\n" +
+                        "VOICE: " + e.client.telephone.tel1 + "\n"
+                    rtn += "END:VCARD\n";
+
+
+                })
+                res.setHeader('Content-disposition', 'attachment; filename=' + "export_clientsV2.vcf");
+                res.setHeader('Content-type', "text/vcard");
+                res.send(rtn)
+            })
+        })
+    }
+}
