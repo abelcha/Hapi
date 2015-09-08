785f28e5a7930f7f13fe0c774fb7b377ec45118d
Abel Chalier <abel@chalier.me>
2015-09-08 20:42:14 +0200

    autocommit

--- front/angular/Filters/relativeDate.js
+++ front/angular/Filters/relativeDate.js
@@ -1,6 +1,13 @@
 angular.module('edison').filter('relativeDate', function() {
     "use strict";
     return function(date, no) {
-        return moment((date + 1370000000) * 1000).fromNow(no).toString()
+    	var d = moment((date + 1370000000) * 1000); 
+    	var l = moment().subtract(4, 'days');
+       	if (d < l) {
+       		return d.format('DD/MM/YY')
+       	} else {
+       		return d.fromNow(no).toString()
+       	}
+       // return moment((date + 1370000000) * 1000).fromNow(no).toString()
     };
 });
