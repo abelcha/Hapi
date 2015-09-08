3f84e3d46a70f01a4d28465ef4ad918b7a248374
Abel Chalier <abel@chalier.me>
2015-09-08 21:14:57 +0200

    autocommit

--- front/assets/dist/all.js
+++ front/assets/dist/all.js
@@ -972,15 +972,25 @@ angular.module('edison').filter('loginify', function() {
 
 angular.module('edison').filter('relativeDate', function() {
     "use strict";
-    return function(date, no) {
-    	var d = moment((date + 1370000000) * 1000); 
-    	var l = moment().subtract(4, 'days');
-       	if (d < l) {
-       		return d.format('DD/MM/YY')
-       	} else {
-       		return d.fromNow(no).toString()
-       	}
-       // return moment((date + 1370000000) * 1000).fromNow(no).toString()
+    return function(date, smallWin) {
+        var d = moment((date + 1370000000) * 1000);
+        var l = moment().subtract(4, 'days');
+        if (d < l) {
+            return d.format('DD/MM/YY')
+        } else {
+            var x = d.fromNow().toString()
+            if (smallWin) {
+                return x.replace('heures', 'H')
+                    .replace('heures', 'H')
+                    .replace('jours', 'J')
+                    .replace('jour', 'J')
+                    .replace('il y a ', '- ')
+                    .replace('dans ', '+ ')
+                    .replace('un ', '1 ')
+            }
+            return x;
+        }
+        // return moment((date + 1370000000) * 1000).fromNow(no).toString()
     };
 });
 
