45d377a60194b9d4b096afef6127c03243126963
Abel Chalier <abel@chalier.me>
2015-09-16 14:02:37 +0200

    autocommit

--- server/models/artisan/validator.js
+++ server/models/artisan/validator.js
@@ -1,5 +1,5 @@
 module.exports = function(schema) {
-    var V1 = requireLocal('config/_artisan_convert_V1.js');
+    var V1 = requireLocal('config/_convert_artisan_V1.js');
 
     var getSubStatus = function() {
         var _ = require('lodash');
