dd2f3891dc6d5a9e70a72a4decf3a0748e9ea7c3
Abel Chalier <abel@chalier.me>
2015-09-16 14:39:26 +0200

    autocommit

--- server/core/__get-cache-list.js
+++ server/core/__get-cache-list.js
@@ -2,6 +2,7 @@
         return function(req, res) {
             new Promise(function(resolve, reject) {
                 redis.get(core.redisCacheListName.envify(), function(err, reply) {
+                    console.log('--->', reply.length)
                     if (!reply || err) {
                         reject('no cache list')
                     }
