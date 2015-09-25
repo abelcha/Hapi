acf3c72efa0c11fde35fefd9c28f4835e59de7c6
Abel Chalier <abel@chalier.me>
2015-09-25 10:23:03 +0200

    autocommit

--- server/core/__full-reload.js
+++ server/core/__full-reload.js
@@ -1,15 +1,16 @@
 module.exports = function(core) {
     return function(res, req) {
+        var _ = require('lodash')
+        console.time('reloadFilters');
+        if (!isWorker) {
+            return edison.worker.createJob({
+                name: 'db',
+                model: core.name,
+                method: 'fullReload',
+                req: _.pick(req, 'query', 'session')
+            })
+        }
         return new Promise(function(resolve, reject) {
-            console.time('reloadFilters');
-            if (!isWorker) {
-                return edison.worker.createJob({
-                    name: 'db',
-                    model: core.name,
-                    method: 'fullReload',
-                    req: _.pick(req, 'query', 'session')
-                })
-            }
             core.model().reloadFilters(function(err) {
                 if (err)
                     reject(err);
