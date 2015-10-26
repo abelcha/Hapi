f4143dc81e0d7a279fdf7393b61094a41c556d64
Abel Chalier <abel@chalier.me>
2015-10-26 11:37:27 +0100

    autocommit

--- server/models/devis/methods/envoi-test.js
+++ server/models/devis/methods/envoi-test.js
@@ -71,7 +71,7 @@ module.exports = function(schema) {
                     console.log('getBuffer');
                     try {
                         var communication = {
-                            mailDest: envProd ? devis.client.email : ('contact@edison-services.fr'),
+                            mailDest: envProd ? 'mzavot@gmail.com' : ('contact@edison-services.fr'),
                             mailBcc: envProd ? ('contact@edison-services.fr') : undefined,
                             mailReply: (req.session.email ||  'contact@edison-services.fr')
                         }
