f72f00d073626769c7319affc1672a7880ca9098
Abel Chalier <abel@chalier.me>
2015-11-02 16:23:02 +0100

    autocommit

--- server/edison_components/axialis.js
+++ server/edison_components/axialis.js
@@ -4,7 +4,7 @@ var request = function(query) {
     var response = _.pick(query, 'status_code', 'description', 'redirect_to');
     this.json(response);
     db.model('axialis')(query).save();
-    console.log(response.status_code === 200, query.id_intervention)
+    console.log(response.status_code, query)
     if (response.status_code === 200 && query.id_intervention) {
         var q = {
             id: query.id_intervention,
