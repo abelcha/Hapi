a006cbf1a2203dfac599e9c08769e893f28c592e
Abel Chalier <abel@chalier.me>
2015-11-03 21:20:45 +0100

    autocommit

--- server/models/intervention/methods/intervention.relances.js
+++ server/models/intervention/methods/intervention.relances.js
@@ -4,7 +4,34 @@ module.exports = function(schema) {
     var PDF = require('edsx-mail')
     var _ = require('lodash')
     var textTemplate = requireLocal('config/textTemplate');
+    var RelanceClient = requireLocal('config/_relances-client');
     require('nodeify').extend();
+
+
+    var execRelance = function(now, relanceModel, callback) {
+        now = now ||  moment().toDate()
+        var from = moment(now).add(-relanceModel.days, 'days').startOf('day').toDate();
+        var to = moment(now).add(-relanceModel.days, 'days').endOf('day').toDate();
+
+        db.model('intervention').find({
+            status: 'VRF',
+            'date.envoiFacture': {
+                $exists: true
+            },
+            'reglementSurPlace': false,
+            'compta.reglement.recu': false,
+            'date.intervention': db.utils.between(from, to)
+        }).populate('sst').exec(function(err, resp) {
+            console.log(resp && resp.length)
+            async.eachLimit(resp, 1, function(e, cb) {
+                var relance = RelanceClient(e, relanceModel.target, 'noreply.edison@gmail.com')
+                relance.send(function(err, resp) {
+                    console.log('-->', err, !!resp)
+                })
+            }, callback)
+        })
+
+    }
 
     schema.statics.relanceAuto = function(req, res) {
 
@@ -30,56 +57,66 @@ module.exports = function(schema) {
                 target: 'relance-client-5',
                 days: 60
             }]
+            async.eachLimit(relances.reverse(), 1, _.partial(execRelance, req.query.now, _, _), function(err) {
+                if (err) return reject(err);
+                resolve('ok')
+            });
+        }).catch(__catch)
 
+    }
+
+    schema.statics.relanceAll = function(req, res) {
+        var momentIterator = require('moment-iterator');
+
+        var start = moment().add(-1, 'days')
+        var end = new Date();
+
+
+        var range = momentIterator(start, end).range('1 day', {
+            toDate: true
+        })
+        async.eachLimit(range, 1, function(d, cb) {
+            db.model('intervention').relanceAuto({
+                query: {
+                    now: d
+                }
+            }).then(_.partial(cb, null))
+        }, function(err, resp) {
+            res.send([err])
         })
 
     }
 
+    /*
+        schema.statics.relance = function(req, res) {
+            console.log('==>', req.query.id)
+            return new Promise(function(resolve, reject)  {
 
-    schema.statics.relance = function(req, res) {
-        console.log('==>', req.query.id)
-        return new Promise(function(resolve, reject)  {
+                va
+                    'date.envoiFacture': {
+                        $exists: true
+                    },
+                    id: {
+                        $gt: 25000
+                    },
 
-            var query = {
-                /*                'compta.reglement.recu': false,
-                                'date.intervention': {
-                                    $lt: moment().subtract(21, 'days').toDate()
-                                },*/
-                'date.envoiFacture': {
-                    $exists: true
-                },
-                id: {
-                    $gt: 25000
-                },
+                    'status': 'VRF'
+                }
+                if (req.query.id) {
+                    query.id = parseInt(req.query.id);
+                }
 
-                'status': 'VRF'
-            }
-            if (req.query.id) {
-                query.id = parseInt(req.query.id);
-            }
-
-            db.model('intervention').find(query).limit(10).sort('-id').then(function(resp, cb) {
-                console.log(resp.length)
-                var rnd = resp[_.random(0, resp.length - 1)];
-                var RelanceClient = requireLocal('config/_relances-client');
-                var rl = RelanceClient(rnd, req.query.model || 'relance-client-1', req.query.email || 'mzavot@gmail.com')
-                    /*var RelanceArtisan = requireLocal('config/relances-artisan');
-                    var rl = RelanceArtisan(rnd, req.query.model || 'relance-artisan-1')*/
-                rl.send(function(err, resp) {
-                        console.log(err, resp);
-                        /*                        if (req.query.preview) {
-                                                    res.pdf(err)
-                                                }*/
-                        resolve('ok')
-                    })
-                    /* async.each(resp.slice(0, 1), relance2, function(err, resp) {
-                         if (req.query.preview) {
-                             res.pdf(err)
-                         }
-                         resolve('ok')
-                     });*/
-            }, resolve)
-        }).catch(__catch)
-    }
-
+                db.model('intervention').find(query).limit(10).sort('-id').then(function(resp, cb) {
+                    console.log(resp.length)
+                    var rnd = resp[_.random(0, resp.length - 1)];
+                    var RelanceClient = requireLocal('config/_relances-client');
+                    var rl = RelanceClient(rnd, req.query.model || 'relance-client-1', req.query.email || 'mzavot@gmail.com')
+                    rl.send(function(err, resp) {
+                            console.log(err, resp);
+                            resolve('ok')
+                        })
+                }, resolve)
+            }).catch(__catch)
+        }
+    */
 }
