57ef947c21a4656dc6690e5731a61982096ca8a0
Abel Chalier <abel@chalier.me>
2015-11-30 07:05:59 +0100

    autocommit

--- server/models/document/methods/document.upload-scans.js
+++ server/models/document/methods/document.upload-scans.js
@@ -1,70 +1,73 @@
    module.exports = function(schema) {
-       var _ = require('lodash')
-       var moment = require('moment')
-       var request = require('request')
-       var async = require('async')
-       var fs = require('fs')
+    var _ = require('lodash')
+    var moment = require('moment')
+    var request = require('request')
+    var async = require('async')
+    var fs = require('fs')
 
-       var getPage = function(filepath, page, cb) {
-           var fs = require('fs')
-           var scissors = require('scissors');
-           var finalBuffer = [];
-           var stream = scissors(filepath).pages(page).pdfStream()
-           stream.on('data', function(data) {
-               finalBuffer.push(data);
-           }).on('end', function() {
-               return cb(null, Buffer.concat(finalBuffer))
-           })
-       }
+    var getPage = function(filepath, page, cb) {
+      var fs = require('fs')
+      var scissors = require('scissors');
+      var finalBuffer = [];
+      var stream = scissors(filepath).pages(page).pdfStream()
+      stream.on('data', function(data) {
+        console.log('YES DATA', data && data.length)
+        finalBuffer.push(data);
+      }).on('end', function() {
+        console.log('EN DATA')
 
-       schema.statics.uploadScans = function(req, res) {
-           return new Promise(function(resolve, reject) {
-               if (!req.files || !req.files.file || !req.files.file.buffer || !req.files.file.extension) {
-                   return reject("Invalid File");
-               }
-               if (req.files.file.size > 5000000)
-                   return reject("File is too big");
+        return cb(null, Buffer.concat(finalBuffer))
+      })
+    }
 
-
-               var fs = require('fs')
-               var uuid = require('uuid')
-               var filepath = '/tmp/' + uuid.v4() + '.pdf';
-               fs.writeFileSync(filepath, req.files.file.buffer);
+    schema.statics.uploadScans = function(req, res) {
+      return new Promise(function(resolve, reject) {
+        if (!req.files || !req.files.file || !req.files.file.buffer || !req.files.file.extension) {
+          return reject("Invalid File");
+        }
+        if (req.files.file.size > 5000000)
+          return reject("File is too big");
 
 
+        var fs = require('fs')
+        var uuid = require('uuid')
+        var filepath = '/tmp/' + uuid.v4() + '.pdf';
+        fs.writeFileSync(filepath, req.files.file.buffer);
 
-               req.body.ids = JSON.parse(req.body.ids)
-               req.body.date = JSON.parse(req.body.date)
-               db.model('intervention').find({
-                       sst: {
-                           $in: _.pluck(req.body.ids, 'id')
-                       },
-                       'compta.paiement.historique': {
-                           $elemMatch: {
-                               dateFlush: req.body.date,
-                               mode: 'CHQ'
-                           }
-                       }
-                   }).then(function(resp) {
-                       var i = 0;
 
-                       async.eachLimit(resp, 5, function(e, callback) {
-                               var flush = _.find(e.compta.paiement.historique, 'dateFlush', new Date(req.body.date));
-                               flush.numeroCheque = (_.find(req.body.ids, 'id', e.sst) || {}).numeroCheque
-                               getPage(filepath, ++i, function(err, buffer) {
-                                   document.upload({
-                                       filename: '/V2_PRODUCTION/intervention/' + e.id + '/' + 'Lettre-cheque-' + flush.numeroCheque + '.pdf',
-                                       data: buffer
-                                   }).then(function(resp) {
-                                       e.save(callback)
-                                   }, callback)
-                               })
 
-                           }, function() {
-                               resolve('ok')
-                           })
-                   })
-               resolve('ok')
-           }).catch(__catch);
-       }
+        req.body.ids = JSON.parse(req.body.ids)
+        req.body.date = JSON.parse(req.body.date)
+        db.model('intervention').find({
+          sst: {
+            $in: _.pluck(req.body.ids, 'id')
+          },
+          'compta.paiement.historique': {
+            $elemMatch: {
+              dateFlush: req.body.date,
+              mode: 'CHQ'
+            }
+          }
+        }).then(function(resp) {
+          var i = 0;
+
+          async.eachLimit(resp, 5, function(e, callback) {
+            var flush = _.find(e.compta.paiement.historique, 'dateFlush', new Date(req.body.date));
+            flush.numeroCheque = (_.find(req.body.ids, 'id', e.sst) || {}).numeroCheque
+            getPage(filepath, ++i, function(err, buffer) {
+              document.upload({
+                filename: '/V2_PRODUCTION/intervention/' + e.id + '/' + 'Lettre-cheque-' + flush.numeroCheque + '.pdf',
+                data: buffer
+              }).then(function(resp) {
+                e.save(callback)
+              }, callback)
+            })
+
+          }, function() {
+            resolve('ok')
+          })
+        })
+        resolve('ok')
+      }).catch(__catch);
+    }
    }
