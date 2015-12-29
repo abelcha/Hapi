cd996b45932b04ae8152f2574b064631c5e6fc89
Abel Chalier <abel@chalier.me>
2015-12-29 14:43:19 +0100

    autocommit

--- server/core/core.reload-filters.js
+++ server/core/core.reload-filters.js
@@ -1,6 +1,7 @@
 module.exports = function(core) {
     var _ = require('lodash')
     return function(query, cb, a, b) {
+        console.log('here')
         var updateFactory = function(obj) {
             return function(cb) {
                 core.model().update(obj.query, obj.update, {
@@ -15,11 +16,17 @@ module.exports = function(core) {
                 cb = query;
                 query = {}
             }
+            console.log('here2')
+
             var fltrs = FiltersFactory(core.name).getAllFilters();
 
+            console.log('here3')
             var updates = {};
+            var __updates = {}
             _.each(fltrs, function(e) {
+                console.log('hereloop', !!e.stats, !!e.match)
                 if (e.stats !== false && e.match) {
+                    console.log('trigger')
                     var match = typeof e.match === 'function' ? e.match() : e.match;
                     var field = 'cache.f.' + e.short_name;
                     var tmp = {
@@ -30,14 +37,16 @@ module.exports = function(core) {
                     }
                     tmp.update.$set[field] = 1;
                     tmp.query['$or'] = [{}, {}]
-                    tmp.query['$or'][0][field] = 0;
+                   /* tmp.query['$or'][0][field] = 0;
                     tmp.query['$or'][1][field] = {
                         $exists: false
-                    };
+                    };*/
+                    __updates[e.short_name] = tmp
                     updates[e.short_name] = updateFactory(tmp)
                 }
             });
-            console.log(JSON.stringify(updates, null, 2))
+
+            console.log(JSON.stringify(__updates, null, 2))
             core.model().update(query, {
                 $set: {
                     'cache.f': {}
