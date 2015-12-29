a28a77925b5d97b466002cbefcc2bb8310a92e1d
Abel Chalier <abel@chalier.me>
2015-12-29 12:32:52 +0100

    autocommit

--- front/angular/Pages/ListeSignalements/liste-signalements.js
+++ front/angular/Pages/ListeSignalements/liste-signalements.js
@@ -3,10 +3,10 @@ var listeSignalements = function(TabContainer, edisonAPI, $rootScope, $scope, $l
     var _this = this;
     _this.tab = TabContainer.getCurrentTab();
     _this.tab.setTitle('Liste Signalements');
+    _this.activeTab = parseInt($location.search().level || 0)
     var q = $location.search();
     edisonAPI.signalement.list($location.search()).then(function(resp) {
         $scope.pl = resp.data;
-        console.log('-->', resp.data)
     })
 
 }
