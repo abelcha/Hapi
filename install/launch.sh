00ccd62e112c222db4cdf82e4dd8c36f0caf210d
Abel Chalier <abel@chalier.me>
2016-01-13 01:14:54 +0100

    autocommit

--- install/launch.sh
+++ install/launch.sh
@@ -1,5 +1,5 @@
 sudo service redis-server start &&
-sudo service mongodb start &&
+sudo service mongod start &&
 sudo service nginx start &&
 sudo service proftpd start && 
 pm2 start ../ecosystem.json
