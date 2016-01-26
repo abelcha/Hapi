0d5d033e25a217bcbaa26cd477f43e1b848dafc5
Abel Chalier <abel.chalier@gmail.com>
2016-01-26 20:58:31 +0100

    autocommit

--- server/models/artisan/methods/artisan.commission.js
+++ server/models/artisan/methods/artisan.commission.js
@@ -4,7 +4,13 @@ module.exports = function(schema) {
 	var moment = require('moment');
 	schema.statics.com = function(req, res) {
 		db.model('artisan').find({
-		
+			nbrIntervention: {
+				$lte: 20,
+				$gt: 0
+			},
+			'date.ajout': {
+				$gt: new Date(2015, 11, 1)
+			}
 		}, {
 			status: 1,
 			nbrIntervention: 1,
@@ -13,15 +19,15 @@ module.exports = function(schema) {
 			var rtn = _(resp).groupBy('nbrIntervention')
 				.value()
 			var x = _.map(rtn, function(e, k) {
-				return {
-					nbrInter: k,
-					total_actif: _.filter(e, 'status', 'ACT').length,
-					total_archive: _.filter(e, 'status', 'ARC').length
-				}
-			})
-			/*x = _.map(x, function(e) {
-				return (_.toArray(e))
-			})*/
+					return {
+						nbrInter: k,
+						total_actif: _.filter(e, 'status', 'ACT').length,
+						total_archive: _.filter(e, 'status', 'ARC').length
+					}
+				})
+				/*x = _.map(x, function(e) {
+					return (_.toArray(e))
+				})*/
 			res.xtable(x);
 			//console.log(x)
 		})
