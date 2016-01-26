5a5ae5c6a3c2ad3515c7e977192cd4db272a3ef7
Abel Chalier <abel.chalier@gmail.com>
2016-01-26 20:45:46 +0100

    autocommit

--- server/edison_components/express-middleware.js
+++ server/edison_components/express-middleware.js
@@ -6,12 +6,32 @@ module.exports = function(express) {
 		return this.send(obj, headers, status);
 	};
 
+	express.response.xtable = function(obj, headers, status) {
+		try {
+			var title = '<tr>' + _.map(_.keys(obj[0]), function(e) {
+				return '<td><strong>' + e + '</strong></td>';
+			})  + '</tr>'
+			var row = _.map(obj, function(e) {
+				return '<td>' + _.toArray(e).join('</td><td>') + '</td>';
+			})
+			var css = '<style> table, td, th {padding: 1px 10px;border: 1px solid black;}</style>'
+			this.send(css + '<table><tr>' + title+ row.join('</tr><tr>') + '</tr></table');
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
 	express.response.table = function(obj, headers, status) {
-		var row = _.map(obj, function(e) {
-			return '<td>' + e.join('</td><td>') + '</td>';
-		})
-		var css = '<style> table, td, th {padding: 1px 10px;border: 1px solid black;}</style>'
-		this.send(css + '<table><tr>' + row.join('</tr><tr>') + '</tr></table');
+		try {
+
+			var row = _.map(obj, function(e) {
+				return '<td>' + e.join('</td><td>') + '</td>';
+			})
+			var css = '<style> table, td, th {padding: 1px 10px;border: 1px solid black;}</style>'
+			this.send(css + '<table><tr>' + row.join('</tr><tr>') + '</tr></table');
+		} catch (e) {
+			console.log(e)
+		}
 	}
 
 	express.response.xls = function(obj, headers, status) {
