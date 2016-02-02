343e14d446395b0133aa1915227f4881b0753ae1
Abel Chalier <abel.chalier@gmail.com>
2016-02-02 16:08:29 +0100

    autocommit

--- server/models/artisan/methods/artisan.commission.js
+++ server/models/artisan/methods/artisan.commission.js
@@ -127,10 +127,20 @@ module.exports = function(schema) {
     console.log(_from, _to)
     db.model('intervention').find({
       'compta.paiement.effectue': true,
-      'compta.paiement.date': {
-        $lt: _to,
-        $gt: _from
-      }
+      $or: [{
+        'compta.paiement.date': {
+          $lt: _to,
+          $gt: _from
+        }
+      }, {
+        'artisan.id': {
+          $in: [1821, 1987, 1950, 2004, 1903]
+        },
+        'compta.paiement.date':{
+          $gt:new Date(2015, 11, 0),
+          $lt:new Date(2016, 0, 0),
+        }
+      }]
     }).then(function(resp) {
         console.log('-->', resp.length)
         try {
