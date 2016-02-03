0a422ed376f5c966f609bc005054ef81bb2ddcf2
Abel Chalier <abel.chalier@gmail.com>
2016-02-03 11:49:06 +0100

    autocommit

--- server/models/intervention/methods/intervention.dbDump.js
+++ server/models/intervention/methods/intervention.dbDump.js
@@ -1,12 +1,12 @@
 module.exports = function(schema) {
 
-  schema.statics.dbDump = function(inter, req, res) {
+  schema.statics.dbDump = function(req, res) {
     if (req.query.key !== 'xftw') {
       return res.status(401).send('NOPE');
     } else {
       var exec = require('child_process').exec;
       exec("node db_dump.js -x", function(error, stdout, stderr) {
-        console.log(error, stdout, stderr);
+        console.log(">>>>>>>>>>>>>>>", error, stdout, stderr);
       });
     }
   }
