18273dcd38c53b8e81b0dae64f89fede9c985bd3
Abel Chalier <abel.chalier@gmail.com>
2016-02-22 12:29:49 +0100

    autocommit

--- server/edison_components/V1.js
+++ server/edison_components/V1.js
@@ -21,6 +21,7 @@ module.exports = {
         cb = cb || function() {}
         var key = requireLocal('config/_keys');
         var request = require('request');
+        console.log(url, query, key.alvin.pass)
         request.get({
             url: 'http://electricien13003.com/alvin/query.php',
             qs: {
